1f44eb07621d68afa80549c5ab650396fd60b785
Daniel Suárez Labena <alu0101040882@ull.edu.es>
2024-03-06 13:42:04 +0000

    update install

--- setup.py
+++ setup.py
@@ -1,6 +1,28 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-import setuptools
 
-if __name__ == "__main__":
-    setuptools.setup()
+# Debugging: Print found packages
+found_packages = find_packages(where='src')
+print("Found packages:", found_packages)
+
+setup(
+    name='meteorology_calculator',
+    version='0.1',
+    package_dir={'': 'src'},
+    packages=[
+        'meteorology_calculator',  # El paquete raíz dentro de src
+        'meteorology_calculator.anomaly',  # Subpaquete anomaly
+        'meteorology_calculator.coldwaves',  # Subpaquete coldwaves
+        'meteorology_calculator.heatwaves',  # Subpaquete heatwaves
+        'meteorology_calculator.plotting'
+        'meteorology_calculator.z_score'
+    ],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+    ],
+    description='Module for meteorology calculations',
+    author='Daniel',
+    author_email='dsuarezl@ull.edu.es',
+)
