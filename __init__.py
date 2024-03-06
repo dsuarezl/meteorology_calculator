e7b14085644bc2d6e3b460b94117657660fb21b4
Daniel Suárez Labena <alu0101040882@ull.edu.es>
2024-03-06 11:43:00 +0000

    updated installation

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# from .heatwaves.heatwave_absolute_calculator import HeatwavesAbsoluteCalculator
-# from .heatwaves.heatwave_percentile_calculator import HeatwavesPercentileCalculator
-# from .heatwaves.heatwave_daily_percentile_calculator import HeatwavesDailyPercentileCalculator
-
-# from .z_score.z_score_all_data import ZScoreAllData
-# from .z_score.z_score_previous_data import ZScorePrevData
--- setup.py
+++ setup.py
@@ -3,16 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name='meteorology_calculator',
     version='0.1',
-    packages=find_packages(),
+    packages=[
+        'meteorology_calculator', 
+        'meteorology_calculator.anomaly',
+        'meteorology_calculator.coldwaves',
+        'meteorology_calculator.heatwaves',
+        'meteorology_calculator.plotting',
+        'meteorology_calculator.z_score'
+    ],
     install_requires=[
         'numpy',
         'pandas',
         'scikit-learn',
     ],
     description='Module for meteorology calculations',
-    # long_description=open('README.md').read(),
-    # long_description_content_type='text/markdown',
     author='Daniel',
     author_email='dsuarezl@ull.edu.es',
-    # url='http://example.com/meteorology_calculator',  # Optional
 )
