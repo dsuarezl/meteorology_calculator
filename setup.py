17ae2d92053e15d79054e21108b08ced14b3a3b7
Daniel Suárez Labena <alu0101040882@ull.edu.es>
2024-03-06 13:28:29 +0000

    update

--- setup.py
+++ setup.py
@@ -1,21 +1,6 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
 
+import setuptools
 
-# Debugging: Print found packages
-found_packages = find_packages(where='src')
-print("Found packages:", found_packages)
-
-setup(
-    name='meteorology_calculator',
-    version='0.1',
-    package_dir={'': 'src'},
-    packages=found_packages,
-    install_requires=[
-        'numpy',
-        'pandas',
-        'scikit-learn',
-    ],
-    description='Module for meteorology calculations',
-    author='Daniel',
-    author_email='dsuarezl@ull.edu.es',
-)
+if __name__ == "__main__":
+    setuptools.setup()
